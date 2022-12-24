f0f3100eac6b4ef8e072cf85005f479549f6de5d
youssefkamel1214 <youssefmk1214@gmail.com>
2022-12-25 00:26:39 +0200

    fixing error of am and pm Calender

--- app/src/main/java/com/youssef/cloath_store/Constants.java
+++ app/src/main/java/com/youssef/cloath_store/Constants.java
@@ -38,7 +38,7 @@ public class Constants {
         return bitMapData;
     }
     public  static Calendar return_hour_to_zero(Calendar calendar){
-        calendar.set(Calendar.HOUR, 0);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
         calendar.set(Calendar.MINUTE, 0);
         calendar.set(Calendar.SECOND, 0);
         calendar.set(Calendar.MILLISECOND, 0);
